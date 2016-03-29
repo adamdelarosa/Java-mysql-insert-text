880e3815bf352367ef8fb2036253b43c4773eb08
ROSA <gojiradam@gmail.com>
2016-03-29 19:46:02 +0200

    info settings

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,82 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
- * Created by ROSA on 3/29/16.
+ * Created by ROSA on 3/28/16.
  */
 public class Main {
+
+    static final String JDBC_DRIVER_USE = "com.mysql.jdbc.Driver";
+    static final String DATABASE_URL = "jdbc:mysql://localhost/TEXTDATA";
+    static final String USERNAME = "username";
+    static final String PASSWORD = "password";
+
+
     public static void main(String args[]){
+        printer();
+        mysqlInsert();
+    }
+
+    public static void printer(){
         System.out.println("Hello");
+    }
+    public static void mysqlInsert(){
+        Connection conn = null;
+        Statement stmt = null;
+        try{
+            //Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //Open a connection
+            System.out.println("Connecting to a selected database...");
+            conn = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+            System.out.println("Connected database successfully...");
+
+            //Execute a query
+            System.out.println("Inserting records into the table...");
+            stmt = conn.createStatement();
+
+            String sql = null;
+
+            //Create DB:
+            /* sql = "CREATE DATABASE TEXTDATA";
+            stmt.executeUpdate(sql);*/
+
+            //Create Table:
+            sql = "CREATE TABLE TEXT_DATA_TABLE " + "(TEXT BLOB(1000))";
+            stmt.executeUpdate(sql);
+
+            //add record
+            //sql = "INSERT INTO Registration " +  "VALUES (102, 'Zaid', 'Khan', 30)";
+            //stmt.executeUpdate(sql);
+
+
+
+            System.out.println("Database.");
+
+
+
+            System.out.println("Inserted records into the table...");
+
+        }catch(SQLException se){
+            se.printStackTrace();
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if(stmt!=null)
+                    conn.close();
+            }catch(SQLException se){
+            }
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
+        System.out.println("Goodbye!");
     }
 }
